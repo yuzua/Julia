e77e85a8848e54211763f9497efc52a82688cdb4
ゆずき <k.saito70329@gmail.com>
2021-08-23 13:57:26 +0900

    [add] practice_problem

---
+++ practice_problem/problem8_9.jl
@@ -0,0 +1,6 @@
+list_a = [10, 20, 30, 40, 50]
+list_b = [22, 33, 44, 55, 66]
+list_c = list_a .+ list_b
+println("list_a = $(list_a)")
+println("list_b = $(list_b)")
+println("list_a + list_b = $(list_c)")
