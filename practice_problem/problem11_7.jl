42f457260da71a0d0e4a5fb3f5ba72ac3ab263fa
ゆずき <k.saito70329@gmail.com>
2021-08-28 15:21:51 +0900

    [add] practice_problem

--- practice_problem/problem11_7.jl
+++ practice_problem/problem11_7.jl
@@ -5,16 +5,22 @@ struct Student
     _number
     _score
 end
+
 function three_means(list)
     return mean(list._score)
 end
+
 function three_add(list)
-    return sum(list._score)
+    return sum(list)
 end
+
 function main()
-    student_list = [[Student("A",001,[89,65,88])], [Student("B",002,[80,95,64])], [Student("C",003,[70,80,98])]]
-    for i in range(student_list)
-        println("$(student_list[i][2])番 $(student_list[i][1])さん 平均点$(three_means(student_list[i][3]))")
+    student_a = Student("A",001,[89,65,88])
+    student_b = Student("B",002,[80,95,64])
+    student_c = Student("C",003,[70,80,98])
+    student_list = (student_a, student_b, student_c)
+    for i = 1:length(student_list)
+        println("$(student_list[i]._number)番 $(student_list[i]._name)さん 平均点$(round(three_means(student_list[i]),digits=2))")
     end
 end
 
--- sample.jl
+++ sample.jl
@@ -1,9 +1,20 @@
-# list = Dict(1=>20, 10=>5)
-# # list_a = collect(values(list)) * collect(keys(list))
-# println(collect(values(list)) * collect(keys(list)))
-dict = Dict("1" => 1, "10" => 2, "100" => 3)
-sum = 0
-for i in keys(dict)
-    global sum += parse(Int, i) * dict[i]
+using Statistics
+
+struct Student
+    _name
+    _number
+    _score
 end
-println(sum)
+
+function three_means(list)
+    return mean(list._score)
+end
+
+student_a = Student("a", 100 ,[1,2,3])
+student_b = Student("b", 200, [1,3,5])
+student_list = (student_a, student_b)
+# for i = 1:length(student_list)
+#     println(student_list[i][1])
+# end
+# println("平均値は$(three_means(student_list[1]))")
+println(student_list[1]._name)
